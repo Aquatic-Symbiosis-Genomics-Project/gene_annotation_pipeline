ff2eb40d6a0bc71f7af25c44e48c8c8ad5dbd6b7
Michael Paulini <mh6@sanger.ac.uk>
2021-08-12 14:34:39 +0100

    Update braker_test.bash
    updated to a current Augustus

--- braker/braker_test.bash
+++ braker/braker_test.bash
@@ -16,7 +16,8 @@ source /software/grit/tools/BRAKER/env.sh
 
 # copy of /software/grit/tools/Augustus/config/*
 export AUGUSTUS_CONFIG_PATH=/lustre/scratch123/tol/teams/grit/mh6/braker/augustus-config
-
+export AUGUSTUS_BIN_PATH=/lustre/scratch123/tol/teams/grit/mh6/Augustus/bin
+export AUGUSTUS_SCRIPTS_PATH=/lustre/scratch123/tol/teams/grit/mh6/Augustus/scripts
 export GENEMARK_PATH=/lustre/scratch123/tol/teams/grit/mh6/gmes_linux_64
 export PERL5LIB=/software/grit/conda/envs/braker/lib/site_perl/5.26.2/
 export PATH=/software/jre1.8.0_131/bin:/software/grit/conda/envs/python3/bin/:$PATH
