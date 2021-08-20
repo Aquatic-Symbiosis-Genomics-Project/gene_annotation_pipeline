58150e16a378f574a255e9154351cc24868574ae
epaule <mh6@sanger.ac.uk>
2021-08-20 16:56:51 +0100

    fix

--- braker/star.bash
+++ braker/star.bash
@@ -26,7 +26,7 @@ mkdir genome_index
 $STAR --runThreadN 12 --runMode genomeGenerate --genomeDir genome_index --genomeFastaFiles genome.fa
 
 # sorted BAM output takes way more memory, so we will sort it afterwards
-$STAR --genomeDir genome_index --runThreadN 12 --readFilesIn $FASTQ1 $FASTQ2 --outFileNamePrefix out --outSAMtype BAM Unsorted --readFilesCommand zcat
+$STAR --genomeDir genome_index --runThreadN 12 --readFilesIn fq1.fq.gz fq2.fq.gz --outFileNamePrefix out --outSAMtype BAM Unsorted --readFilesCommand zcat
 
 # round two with splice junctions from #1
-$STAR --genomeDir genome_index --runThreadN 12 --readFilesIn $FASTQ1 $FASTQ2 --outFileNamePrefix out2 --outSAMtype BAM Unsorted --readFilesCommand zcat --sjdbFileChrStartEnd *.tab
+$STAR --genomeDir genome_index --runThreadN 12 --readFilesIn fq1.fq.gz fq2.fq.gz --outFileNamePrefix out2 --outSAMtype BAM Unsorted --readFilesCommand zcat --sjdbFileChrStartEnd *.tab
