d18ac21f71b49d9b9779961e2d338bc23eaf6e30
Michael Paulini <mh6@sanger.ac.uk>
2021-09-13 15:01:24 +0100

    Update make_hub.bash
    Ksenia found the offending env

--- braker/make_hub.bash
+++ braker/make_hub.bash
@@ -15,7 +15,7 @@ export AUGUSTUS_BIN_PATH=/lustre/scratch123/tol/teams/grit/mh6/Augustus/bin
 export AUGUSTUS_SCRIPTS_PATH=/lustre/scratch123/tol/teams/grit/mh6/Augustus/scripts
 export GENEMARK_PATH=/lustre/scratch123/tol/teams/grit/mh6/gmes_linux_64
 export PERL5LIB=/software/grit/conda/envs/braker/lib/site_perl/5.26.2/
-export PATH=/software/jre1.8.0_131/bin:/software/grit/conda/envs/python3/bin/:$PATH
+export PATH=/software/jre1.8.0_131/bin:$PATH
 export MAKEHUB_PATH=/software/grit/tools/MakeHub
 export SAMTOOLS_PATH=/software/grit/bin
 export GUSHR_PATH=/software/grit/tools/GUSHR
@@ -28,4 +28,4 @@ fi
 mkdir s4_makehub
 cd s4_makehub
 
-/software/grit/conda/envs/braker/bin/python /lustre/scratch123/tol/teams/grit/mh6/MakeHub/make_hub.py -g $GENOME -L $LABEL -l $LABEL -e test@email.ac.uk -X ../s3_braker/braker -b ../s2_alignment/out2Aligned.out.bam ../s2b_alignment/out4Aligned.out.bam
+python /lustre/scratch123/tol/teams/grit/mh6/MakeHub/make_hub.py -g $GENOME -L $LABEL -l $LABEL -e test@email.ac.uk -X ../s3_braker/braker -b ../s2_alignment/out2Aligned.out.bam ../s2b_alignment/out4Aligned.out.bam
